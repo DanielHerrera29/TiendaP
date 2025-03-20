2885373cee53ac010a5a9171bf769cd20ccfb99e
slend <slend@PcSlendy>
2025-03-19 20:14:24 -0500

    no he hecho casi nada

--- src/main/java/co/edu/poli/Ejercicio/model/Componente.java
+++ src/main/java/co/edu/poli/Ejercicio/model/Componente.java
@@ -3,6 +3,8 @@ package co.edu.poli.Ejercicio.model;
 public interface Componente {
 	    void iniciarRegistroTiempo();  
 	    
+	    
+	    
 	    void finalizarRegistroTiempo();  
 	    
 	    long obtenerTiempoTranscurrido();  
