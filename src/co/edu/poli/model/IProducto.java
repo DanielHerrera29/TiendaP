ed3c765076cb695829acb79735ca7ead07aa8041
anyelithjacobo <anyelithjacobo@gmail.com>
2025-04-29 16:37:49 -0500

    refactor a patron proxy

--- src/co/edu/poli/model/IProducto.java
+++ src/co/edu/poli/model/IProducto.java
@@ -4,14 +4,5 @@ public interface IProducto {
     String obtenerDetalles(int usuarioId);
     double obtenerPrecio(int usuarioId);
     String obtenerEspecificaciones(int usuarioId);
-    Proveedor obtenerProveedor(int usuarioId); // Nuevo método
+    Proveedor obtenerProveedor(int usuarioId); 
 }
-// package co.edu.poli.model;
-
-// public interface IProducto {
-//     String obtenerDetalles(int usuarioId);
-//     double obtenerPrecio(int usuarioId);
-//     String obtenerEspecificaciones(int usuarioId);
-//     Proveedor obtenerProveedor();
-//     Proveedor actualizarProveedor(Proveedor proveedor); 
-// }
